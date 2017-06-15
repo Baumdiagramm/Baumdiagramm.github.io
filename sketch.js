5c4d9b2b702b29ce0cc5fd58e1a701f3501ed7e7
Pablofero <pablofrldp@gmail.com>
2017-06-15 18:50:47 +0200

    added some

--- sketch.js
+++ sketch.js
@@ -2,17 +2,19 @@ var N
 var a
 var deg,amount;
 var first;
-var col;
+var decrease;
+var firstSize;
 function setup() {
     createCanvas(800, 400);
     first = true;
-    col=200;
-    N = createInput();
+    decrease=0.5;
+    firstSize=120*3
+    N = createInput();//depth
     N.position(width, 0);
     N.size(30, 15);
     N.value(3);
 
-    a = createInput();
+    a = createInput();//branches
     a.position(width, 20);
     a.size(30, 15);
     a.value(2);
@@ -25,35 +27,36 @@ function draw() {
     deg =PI/a.value();
     amount =.5;// PI / a.value();
     first=true;
-    baum(120*3);
+    //firstSize*pow(decrease,x)=a;
+    //var limi=(log(a/firstSize)/log(decrease))
+    var limi=0;
+    baum(firstSize,decrease,limi);
 }
 
-function baum(len) {
+function baum(len,decrease,limit) {
     // Each branch will be 2/3rds the size of the previous one
 
-    //float sw = map(len,2,120,1,10);
+    //float sw = map(le*n,2,120,1,10);
     //strokeWeight(sw);
     strokeWeight(1);
 
     if (!first) {
         //stroke(random(255),random(255),random(255),random(255));
-        col-=10
         line(0, 0, 0, -len);
         // Move to the end of that line
         translate(0, -len);
     }
     first = false;
 
-    len *= 0.5;
+    len *= decrease;
     // All recursive functions must have an exit condition!!!!
     // Here, ours is when the length of the branch is 2 pixels or less
-    if (len > 30) {
+    if (len > 45){//limit) {
 
       //rotate(HALF_PI/3);
       //  for (var i = 0; i < 2; i++  && degaument() ) {
           for (var j = 1-(deg/PI)*1; j >=-1; j-=(deg/PI)*2) {
             if(j>0){stroke(255,0,0);}else{stroke(0,0,255)}
-            console.log(j);
             push(); // Save the current state of transformation (i.e. where are we now)
             rotate(deg*j); // Rotate by deg
             baum(len); // Ok, now call myself to draw two new branches!!
